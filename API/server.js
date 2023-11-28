ec0d3d5399cb39df5f122b781aa828af4ad6b1eb
Eduard Berrio <genial@duck.com>
2023-11-28 08:48:12 -0500

    fix: route client to run

--- API/server.js
+++ API/server.js
@@ -25,10 +25,10 @@ app.use('/api/users', userRoutes);
 
 if (process.env.MODE_ENV === 'production') {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+  app.use(express.static(path.join(__dirname, '/client/dist')));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
   );
 } else {
   app.get('/', (req, res) => {
