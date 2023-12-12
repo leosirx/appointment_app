dc97b5c729443df1a0ad5d861cc0bc99f23d5db9
Eduard Berrio <genial@duck.com>
2023-12-12 10:16:26 -0500

    design: color buttons

--- client/src/screens/RegisterScreen.jsx
+++ client/src/screens/RegisterScreen.jsx
@@ -108,7 +108,7 @@ const RegisterScreen = () => {
                             </div>
                                 <a href="#" className="text-sm text-gray-500 font-medium text-primary-600 hover:underline dark:text-primary-800">Forgot password?</a>
                             </div>
-                            <button type="submit" className="w-full bg-blue-700 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                            <button type="submit" className="w-full bg-purple-500 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                                 Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
                             </p>
