3fb30d5226f6f36ef1ec0d4d62fc5f2e51e65ec2
Eduard Berrio <ingberrio@yahoo.com>
2023-12-22 18:29:17 -0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://appoint-dev-kghr.1.us-1.fl0.io/',
         changeOrigin: true,
       },
     },
