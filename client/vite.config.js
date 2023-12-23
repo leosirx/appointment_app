368a810d69a463cabd444ab966bfa297b6f9b632
Eduard Berrio <genial@duck.com>
2023-12-23 16:05:15 -0500

    feat: vite.config deploy config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,23 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: { 
+    outDir: "build",
+    chunkSizeWarningLimit: 1600,
+    assetsDir: 'assets',
+    assetsInlineLimit: 4096,
+  },
   server: {
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5000/',
         changeOrigin: true,
       },
     },
-  }
+  },
+  manualChunks: {
+		lodash: ['lodash']
+	},
+  base: '/'
 })
