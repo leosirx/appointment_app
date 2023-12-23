3237c7148436ade1f43b8460d9150253eaef6b92
Eduard Berrio <genial@duck.com>
2023-12-23 08:09:03 -0500

    fix: vercel.json V.1.6

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,10 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: { 
-    chunkSizeWarningLimit: 1600, 
-    rollupOptions: {
-      external: ['@mui/material'],
-    },
+    chunkSizeWarningLimit: 1600,
   },
   server: {
     port: 3000,
